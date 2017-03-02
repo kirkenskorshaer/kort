339c8e83ab9e7d7909a62bffb530a6b39acf1ca5
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-03-02 10:42:14 +0100

    (Android) adding GetRandomString method

--- kort/Android/MedlemskortAndroid/app/src/main/java/kirkenskorshr/medlemskortandroid/MainActivity.java
+++ kort/Android/MedlemskortAndroid/app/src/main/java/kirkenskorshr/medlemskortandroid/MainActivity.java
@@ -1,13 +1,40 @@
 package kirkenskorshr.medlemskortandroid;
 
+import android.net.Uri;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+import kirkenskorshr.medlemskortandroid.Connection.Connection;
+
+public class MainActivity extends AppCompatActivity
+{
+	private Random _random;
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		_random = new Random();
+	}
+
+	private String GetRandomString()
+	{
+		int length = (Math.abs(_random.nextInt()) % 5) + 2;
+
+		String randomString = "";
+		for (int index = 0; index < length; index++)
+		{
+			int randomInt = (Math.abs(_random.nextInt()) % 57) + 65;
+			char randomChar = (char) randomInt;
+
+			randomString += randomChar;
+		}
+
+		return randomString;
+	}
+
 }
