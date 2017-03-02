5cc70501000bff42ac8d996447b84f7d72e8eb71
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-03-02 10:50:37 +0100

    (Android) adding connection logic

--- kort/Android/MedlemskortAndroid/app/src/main/java/kirkenskorshr/medlemskortandroid/MainActivity.java
+++ kort/Android/MedlemskortAndroid/app/src/main/java/kirkenskorshr/medlemskortandroid/MainActivity.java
@@ -14,11 +14,46 @@ import kirkenskorshr.medlemskortandroid.Connection.Connection;
 public class MainActivity extends AppCompatActivity
 {
 	private Random _random;
+	private Connection _connection;
+
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		EditText ipText = (EditText) findViewById(R.id.edit_ip);
+		EditText portText = (EditText) findViewById(R.id.edit_port);
+
+		//ipText.setText("10.10.11.126");
+		ipText.setText("192.168.1.82");
+		portText.setText("997");
+
 		_random = new Random();
+	}
+
+	public void connect(View view)
+	{
+		EditText ipText = (EditText) findViewById(R.id.edit_ip);
+		EditText portText = (EditText) findViewById(R.id.edit_port);
+
+		String ip = ipText.getText().toString();
+		int port = Integer.parseInt(portText.getText().toString());
+
+		_connection = new Connection(ip, port);
+		_connection.connect();
+
+		send("Name:" + android.os.Build.MODEL);
+	}
+
+	public void send(View view)
+	{
+		String randomString = GetRandomString();
+		send("Code:" + randomString);
+	}
+
+	private Boolean send(String message)
+	{
+		return _connection.send(message);
 	}
 
 	private String GetRandomString()
@@ -37,4 +72,8 @@ public class MainActivity extends AppCompatActivity
 		return randomString;
 	}
 
+	public void disconnect(View view)
+	{
+		_connection.disconnect();
+	}
 }
