9753304ea01751e77639e78aaffdca2379a0755a
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-03-02 11:15:26 +0100

    (Scanner) added ScannerConnectionFinder with android scanner

---
+++ kort/Scanner/AndroidConnection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Scanner
+{
+	public class AndroidConnection : AbstractScannerConnection
+	{
+		private Socket _socket;
+
+		private BackgroundWorker _dataReader = new BackgroundWorker();
+
+		public AndroidConnection(Socket socket)
+		{
+			_socket = socket;
+
+			_dataReader.DoWork += Read;
+			_dataReader.RunWorkerCompleted += ReadEnd;
+
+			_dataReader.RunWorkerAsync();
+		}
+
+		private void Read(object sender, DoWorkEventArgs e)
+		{
+			NetworkStream networkStream = new NetworkStream(_socket);
+
+			StreamReader reader = new StreamReader(networkStream);
+
+			string read = "";
+			int nextChar = 0;
+			while (_socket.Connected && nextChar != -1)
+			{
+				nextChar = reader.Read();
+
+				if (nextChar == '§')
+				{
+					string[] keyValue = read.Split(':');
+
+					if (keyValue.Length == 2)
+					{
+						string key = keyValue[0];
+						string value = keyValue[1];
+
+						RetreiveDataFromScanner(key, value);
+					}
+
+					read = "";
+				}
+				else
+				{
+					read += (char)nextChar;
+				}
+			}
+		}
+
+        private void ReadEnd(object sender, RunWorkerCompletedEventArgs e)
+		{
+			OnDisconnect(new EventArgs());
+		}
+	}
+}
---
+++ kort/Scanner/ScannerConnectionFinder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Scanner
+{
+	public class ScannerConnectionFinder
+	{
+		public event EventHandler ScannerConnected;
+
+		private int _port = 997;
+		private IPAddress _ipaddress = IPAddress.Any;
+
+		private BackgroundWorker _androidConnectionListener = new BackgroundWorker();
+
+		public ScannerConnectionFinder()
+		{
+			_androidConnectionListener.DoWork += ListenForAndroidConnection;
+			_androidConnectionListener.RunWorkerCompleted += ReturnConnection;
+			_androidConnectionListener.RunWorkerAsync();
+		}
+
+		private void ReturnConnection(object sender, RunWorkerCompletedEventArgs e)
+		{
+			OnScannerConnected((AbstractScannerConnection)e.Result);
+			_androidConnectionListener.RunWorkerAsync();
+		}
+
+		private void ListenForAndroidConnection(object sender, DoWorkEventArgs e)
+		{
+			TcpListener tcpListener = new TcpListener(_ipaddress, _port);
+			tcpListener.Start();
+
+			Socket clientSocket = tcpListener.AcceptSocket();
+
+			tcpListener.Stop();
+
+			e.Result = new AndroidConnection(clientSocket);
+		}
+
+		private void OnScannerConnected(AbstractScannerConnection scannerConnection)
+		{
+			EventHandler handler = ScannerConnected;
+			if (handler != null)
+			{
+				handler(this, scannerConnection);
+			}
+		}
+	}
+}
