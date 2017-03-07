5cc712250b462675620f7f85b08ff9746a07cf63
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-03-07 11:07:25 +0100

    (Utilities) created project

---
+++ kort/Utilities/ReflectionHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Utilities
+{
+	public static class ReflectionHelper
+    {
+		public static List<Type> GetChildTypes(Type parentType)
+		{
+			Assembly assembly = parentType.Assembly;
+
+			Type[] allTypes = assembly.GetTypes();
+
+			List<Type> typesInheritingFromParent = allTypes.Where(type => parentType.IsAssignableFrom(type) && type != parentType).ToList();
+
+			return typesInheritingFromParent;
+		}
+	}
+}
