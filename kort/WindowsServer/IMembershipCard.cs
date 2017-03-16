a0770245d111f805e2b7550afaba5bf072239a47
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-03-16 13:54:20 +0100

    (WindowsServer) defining rest api

--- kort/WindowsServer/IMembershipCard.cs
+++ kort/WindowsServer/IMembershipCard.cs
@@ -2,6 +2,7 @@
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using WindowsServer.DataContract;
+using WindowsServer.DataContract.Alert;
 
 namespace WindowsServer
 {
@@ -9,7 +10,66 @@ namespace WindowsServer
 	public interface IMembershipCard
 	{
 		[OperationContract]
-		[WebInvoke(Method = "Get", UriTemplate = "GetMembers/", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
-		List<Member> GetMembers(string id);
+		[WebInvoke(Method = "GET", UriTemplate = "Members/CardId/{cardId}", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		List<Member> GetMembersByCardId(string cardId);
+
+		[OperationContract]
+		[WebInvoke(Method = "GET", UriTemplate = "Members/NickName/{nickName}", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		List<Member> GetMembersByNickName(string nickName);
+
+		[OperationContract]
+		[WebInvoke(Method = "GET", UriTemplate = "Members/ToAlert", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		List<Member> GetMembersToAlert();
+
+		[OperationContract]
+		[WebInvoke(Method = "POST", UriTemplate = "Member", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		InsertResult InsertMember(Member member);
+
+		[OperationContract]
+		[WebInvoke(Method = "PUT", UriTemplate = "Member", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		void UpdateMember(Member member);
+
+		[OperationContract]
+		[WebInvoke(Method = "DELETE", UriTemplate = "Member", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		void DeleteMember(Member member);
+
+
+		[OperationContract]
+		[WebInvoke(Method = "GET", UriTemplate = "Alerts/VisitTimeAlert/{memberid}", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		List<VisitTimeAlert> GetAlertsOnMember(string memberid);
+
+		[OperationContract]
+		[WebInvoke(Method = "POST", UriTemplate = "Alert/VisitTimeAlert", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		InsertResult InsertAlert(VisitTimeAlert alert);
+
+		[OperationContract]
+		[WebInvoke(Method = "DELETE", UriTemplate = "Alert/VisitTimeAlert", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		void DeleteAlert(VisitTimeAlert alert);
+
+
+		[OperationContract]
+		[WebInvoke(Method = "GET", UriTemplate = "Services/{memberid}", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		List<Service> GetServicesOnMember(string memberid);
+
+		[OperationContract]
+		[WebInvoke(Method = "POST", UriTemplate = "Service", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		InsertResult InsertService(Service service);
+
+		[OperationContract]
+		[WebInvoke(Method = "PUT", UriTemplate = "Service", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		void UpdateService(Service service);
+
+		[OperationContract]
+		[WebInvoke(Method = "DELETE", UriTemplate = "Service", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		void DeleteService(Service service);
+
+
+		[OperationContract]
+		[WebInvoke(Method = "GET", UriTemplate = "Visits/{memberid}", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		List<Visit> GetVisitsOnMember(string memberid);
+
+		[OperationContract]
+		[WebInvoke(Method = "POST", UriTemplate = "Visit", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+		InsertResult InsertVisit(Visit visit);
 	}
 }
--- kort/WindowsServer/MembershipCard.svc.cs
+++ kort/WindowsServer/MembershipCard.svc.cs
@@ -1,14 +1,88 @@
-﻿using System.Collections.Generic;
+﻿using Logic.Handler;
+using System.Collections.Generic;
+using System.Linq;
 using WindowsServer.DataContract;
-using WindowsServer.Logic;
+using WindowsServer.DataContract.Alert;
 
 namespace WindowsServer
 {
 	public class MembershipCard : IMembershipCard
 	{
-		public List<Member> GetMembers(string id)
+		public List<Member> GetMembersByCardId(string cardId)
 		{
-			return MemberHandler.Instance.GetMembers();
+			return MemberHandler.Instance.GetMembersByCardId(cardId).Select(dataMember => Member.FromData(dataMember)).ToList();
+		}
+
+		public List<Member> GetMembersByNickName(string nickName)
+		{
+			return MemberHandler.Instance.GetMembersByNickName(nickName).Select(dataMember => Member.FromData(dataMember)).ToList();
+		}
+
+		public List<Member> GetMembersToAlert()
+		{
+			return MemberHandler.Instance.GetMembersToAlert().Select(dataMember => Member.FromData(dataMember)).ToList();
+		}
+
+		public InsertResult InsertMember(Member member)
+		{
+			return new InsertResult() { Id = MemberHandler.Instance.InsertMember(member.ToData()) };
+		}
+
+		public void UpdateMember(Member member)
+		{
+			MemberHandler.Instance.UpdateMember(member.ToData());
+		}
+
+		public void DeleteMember(Member member)
+		{
+			MemberHandler.Instance.DeleteMember(member.ToData());
+		}
+
+
+		public List<VisitTimeAlert> GetAlertsOnMember(string memberid)
+		{
+			return AlertHandler.Instance.GetAlertsOnMember(memberid).Select(alert => VisitTimeAlert.FromData(alert)).ToList();
+		}
+
+		public InsertResult InsertAlert(VisitTimeAlert alert)
+		{
+			return new InsertResult() { Id = AlertHandler.Instance.InsertAlert(alert.ToData()) };
+		}
+
+		public void DeleteAlert(VisitTimeAlert alert)
+		{
+			AlertHandler.Instance.Delete(alert.ToData());
+		}
+
+		public List<Service> GetServicesOnMember(string memberId)
+		{
+			return ServiceHandler.Instance.GetServicesOnMember(memberId).Select(dataService => Service.FromData(dataService, memberId)).ToList();
+		}
+
+		public InsertResult InsertService(Service service)
+		{
+			return new InsertResult() { Id = ServiceHandler.Instance.InsertService(service.ToData()) };
+		}
+
+		public void UpdateService(Service service)
+		{
+			ServiceHandler.Instance.UpdateService(service.ToData());
+		}
+
+		public void DeleteService(Service service)
+		{
+			ServiceHandler.Instance.DeleteService(service.ToData());
+		}
+
+
+		public List<Visit> GetVisitsOnMember(string memberId)
+		{
+			return VisitHandler.Instance.GetVisitsOnMember(memberId).Select(dataVisit => Visit.FromData(dataVisit)).ToList();
+		}
+
+		public InsertResult InsertVisit(Visit visit)
+		{
+			return new InsertResult() { Id = VisitHandler.Instance.InsertVisit(visit.ToData()) };
 		}
 	}
 }
