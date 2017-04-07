91d823ca2a9749bd319812ce09dc183091a7fb66
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-04-07 13:38:14 +0200

    (ClientLogic) added ticket

---
+++ kort/ClientLogic/CardServer/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClientLogic.CardServer
+{
+	public partial class Ticket
+	{
+		public int UseCount = 0;
+
+		public bool AssumedValid()
+		{
+			DateTime currentTime = DateTime.Now;
+
+			return ValidFrom <= currentTime && ValidTo > currentTime;
+		}
+	}
+}
