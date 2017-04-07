325b4bd654c90970ee12d2ad4b6f5c60d7fd11a9
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-04-07 13:38:03 +0200

    (ClientLogic) CardServerClient now uses tickets

--- kort/ClientLogic/CardServerClient.cs
+++ kort/ClientLogic/CardServerClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Ducksboard.Serializers;
 using System.Configuration;
+using System;
 using ClientLogic.CardServer;
 
 namespace ClientLogic
@@ -9,146 +10,335 @@ namespace ClientLogic
 	public class CardServerClient
 	{
 		private RestClient _client;
+		private Ticket _ticket = null;
 
-		public CardServerClient()
+		private string _userName;
+		private string _password;
+		private string _ip;
+		private TimeSpan _ticketLifeSpan;
+
+		public CardServerClient(string userName, string password, string ip, TimeSpan ticketLifeSpan)
 		{
 			string serverUrl = ConfigurationManager.AppSettings["serverUrl"];
 			_client = new RestClient(serverUrl);
+
+			_userName = userName;
+			_password = password;
+			_ip = ip;
+			_ticketLifeSpan = ticketLifeSpan;
 		}
 
 		public List<Member> GetMembersToAlert()
 		{
-			RestRequest request = new RestRequest("Members/ToAlert", Method.GET);
-			IRestResponse<List<Member>> response = _client.Execute<List<Member>>(request);
+			Func<RestRequest> createRequest = () => new RestRequest("Members/ToAlert", Method.GET);
 
-			return response.Data;
+			Func<RestRequest, IRestResponse<List<Member>>> send = (request) => _client.Execute<List<Member>>(request);
+
+			return SendWithRetry(createRequest, send);
 		}
 
 		public List<Member> GetMembersByCardId(string cardId)
 		{
-			RestRequest request = new RestRequest("Members/CardId/{cardId}", Method.GET);
+			Func<RestRequest> createRequest = () =>
+			{
+				RestRequest request = new RestRequest("Members/CardId/{cardId}", Method.GET);
 
-			request.AddUrlSegment("cardId", cardId);
+				request.AddUrlSegment("cardId", cardId);
 
-			IRestResponse<List<Member>> response = _client.Execute<List<Member>>(request);
+				return request;
+			};
 
-			return response.Data;
+			Func<RestRequest, IRestResponse<List<Member>>> send = (request) => _client.Execute<List<Member>>(request);
+
+			return SendWithRetry(createRequest, send);
 		}
 
 		public List<Member> GetMembersByNickName(string nickName)
 		{
-			RestRequest request = new RestRequest("Members/NickName/{nickName}", Method.GET);
+			Func<RestRequest> createRequest = () =>
+			{
+				RestRequest request = new RestRequest("Members/NickName/{nickName}", Method.GET);
 
-			request.AddUrlSegment("nickName", nickName);
+				request.AddUrlSegment("nickName", nickName);
 
-			IRestResponse<List<Member>> response = _client.Execute<List<Member>>(request);
+				return request;
+			};
 
-			return response.Data;
+			Func<RestRequest, IRestResponse<List<Member>>> send = (request) => _client.Execute<List<Member>>(request);
+
+			return SendWithRetry(createRequest, send);
 		}
-
 
 		public void InsertMember(Member member)
 		{
-			RestRequest request = new RestRequest("Member", Method.POST);
-			request.RequestFormat = DataFormat.Json;
+			Func<RestRequest> createRequest = () =>
+			{
+				RestRequest request = new RestRequest("Member", Method.POST)
+				{
+					RequestFormat = DataFormat.Json,
+					JsonSerializer = new RestSharpDataContractJsonSerializer(),
+				};
 
-			request.AddBody(member);
+				request.AddBody(member);
 
-			IRestResponse<InsertResult> response = _client.Post<InsertResult>(request);
+				return request;
+			};
 
-			member.Id = response.Data.Id;
+			Func<RestRequest, IRestResponse<InsertResult>> send = (request) => _client.Post<InsertResult>(request);
+
+			InsertResult result = SendWithRetry(createRequest, send);
+
+			member.Id = result.Id;
 		}
 
 		public List<VisitTimeAlert> GetAlertsOnMember(string memberId)
 		{
-			RestRequest request = new RestRequest("Alerts/VisitTimeAlert/{memberid}", Method.GET);
+			Func<RestRequest> createRequest = () =>
+			{
+				RestRequest request = new RestRequest("Alerts/VisitTimeAlert/{memberid}", Method.GET);
 
-			request.AddUrlSegment("memberid", memberId);
+				request.AddUrlSegment("memberid", memberId);
 
-			IRestResponse<List<VisitTimeAlert>> response = _client.Execute<List<VisitTimeAlert>>(request);
+				return request;
+			};
 
-			return response.Data;
+			Func<RestRequest, IRestResponse<List<VisitTimeAlert>>> send = (request) => _client.Execute<List<VisitTimeAlert>>(request);
+
+			return SendWithRetry(createRequest, send);
 		}
 
 		public List<Service> GetServicesOnMember(string memberId)
 		{
-			RestRequest request = new RestRequest("Services/{memberid}", Method.GET);
+			Func<RestRequest> createRequest = () =>
+			{
+				RestRequest request = new RestRequest("Services/{memberid}", Method.GET);
 
-			request.AddUrlSegment("memberid", memberId);
+				request.AddUrlSegment("memberid", memberId);
 
-			IRestResponse<List<Service>> response = _client.Execute<List<Service>>(request);
+				return request;
+			};
 
-			return response.Data;
+			Func<RestRequest, IRestResponse<List<Service>>> send = (request) => _client.Execute<List<Service>>(request);
+
+			return SendWithRetry(createRequest, send);
 		}
 
 		public List<Visit> GetVisitsOnMember(string memberId)
 		{
-			RestRequest request = new RestRequest("Visits/{memberid}", Method.GET);
+			Func<RestRequest> createRequest = () =>
+			{
+				RestRequest request = new RestRequest("Visits/{memberid}", Method.GET);
 
-			request.AddUrlSegment("memberid", memberId);
+				request.AddUrlSegment("memberid", memberId);
 
-			IRestResponse<List<Visit>> response = _client.Execute<List<Visit>>(request);
+				return request;
+			};
 
-			return response.Data;
+			Func<RestRequest, IRestResponse<List<Visit>>> send = (request) => _client.Execute<List<Visit>>(request);
+
+			return SendWithRetry(createRequest, send);
 		}
 
 		public void InsertService(Service service)
 		{
-			RestRequest request = new RestRequest("Service", Method.POST);
-			request.RequestFormat = DataFormat.Json;
+			Func<RestRequest> createRequest = () =>
+			{
+				RestRequest request = new RestRequest("Service", Method.POST)
+				{
+					RequestFormat = DataFormat.Json,
+					JsonSerializer = new RestSharpDataContractJsonSerializer(),
+				};
 
-			request.AddBody(service);
+				request.AddBody(service);
 
-			IRestResponse<InsertResult> response = _client.Post<InsertResult>(request);
+				return request;
+			};
 
-			service.Id = response.Data.Id;
+			Func<RestRequest, IRestResponse<InsertResult>> send = (request) => _client.Post<InsertResult>(request);
+
+			service.Id = SendWithRetry(createRequest, send).Id;
 		}
 
 		public void InsertVisit(Visit visit)
 		{
-			RestRequest request = new RestRequest("Visit", Method.POST);
-			request.RequestFormat = DataFormat.Json;
-			request.JsonSerializer = new RestSharpDataContractJsonSerializer();
+			Func<RestRequest> createRequest = () =>
+			{
+				RestRequest request = new RestRequest("Visit", Method.POST)
+				{
+					RequestFormat = DataFormat.Json,
+					JsonSerializer = new RestSharpDataContractJsonSerializer(),
+				};
 
-			request.AddBody(visit);
+				request.AddBody(visit);
 
-			IRestResponse<InsertResult> response = _client.Post<InsertResult>(request);
+				return request;
+			};
 
-			visit.Id = response.Data.Id;
+			Func<RestRequest, IRestResponse<InsertResult>> send = (request) => _client.Post<InsertResult>(request);
+
+			visit.Id = SendWithRetry(createRequest, send).Id;
 		}
 
 		public void InsertAlert(VisitTimeAlert alert)
 		{
-			RestRequest request = new RestRequest("Alert/VisitTimeAlert", Method.POST);
-			request.RequestFormat = DataFormat.Json;
-			request.JsonSerializer = new RestSharpDataContractJsonSerializer();
+			Func<RestRequest> createRequest = () =>
+			{
+				RestRequest request = new RestRequest("Alert/VisitTimeAlert", Method.POST)
+				{
+					RequestFormat = DataFormat.Json,
+					JsonSerializer = new RestSharpDataContractJsonSerializer(),
+				};
 
-			request.AddBody(alert);
+				request.AddBody(alert);
 
-			IRestResponse<InsertResult> response = _client.Post<InsertResult>(request);
+				return request;
+			};
 
-			alert.Id = response.Data.Id;
+			Func<RestRequest, IRestResponse<InsertResult>> send = (request) => _client.Post<InsertResult>(request);
+
+			alert.Id = SendWithRetry(createRequest, send).Id;
 		}
 
 		public void UpdateMember(Member member)
 		{
-			RestRequest request = new RestRequest("Member", Method.PUT);
-			request.RequestFormat = DataFormat.Json;
+			Func<RestRequest> createRequest = () =>
+			{
+				RestRequest request = new RestRequest("Member", Method.PUT)
+				{
+					RequestFormat = DataFormat.Json,
+					JsonSerializer = new RestSharpDataContractJsonSerializer(),
+				};
+				request.AddBody(member);
 
-			request.AddBody(member);
+				return request;
+			};
 
-			IRestResponse response = _client.Put(request);
+			Func<RestRequest, IRestResponse> send = (request) => _client.Put(request);
+
+			SendWithRetry(createRequest, send);
 		}
 
 		public void UpdateService(Service service)
 		{
-			RestRequest request = new RestRequest("Service", Method.PUT);
-			request.RequestFormat = DataFormat.Json;
-			request.JsonSerializer = new RestSharpDataContractJsonSerializer();
+			Func<RestRequest> createRequest = () =>
+			{
+				RestRequest request = new RestRequest("Service", Method.PUT)
+				{
+					RequestFormat = DataFormat.Json,
+					JsonSerializer = new RestSharpDataContractJsonSerializer(),
+				};
+				request.AddBody(service);
 
-			request.AddBody(service);
+				return request;
+			};
 
-			IRestResponse response = _client.Put(request);
+			Func<RestRequest, IRestResponse> send = (request) => _client.Put(request);
+
+			SendWithRetry(createRequest, send);
+		}
+
+		public RestResponseType SendWithRetry<RestResponseType>(Func<RestRequest> createRequest, Func<RestRequest, IRestResponse<RestResponseType>> send, bool isRetry = false)
+		{
+			RestRequest request = createRequest();
+			AddTicketHeader(request);
+			IRestResponse<RestResponseType> response = send(request);
+
+			if (isRetry)
+			{
+				return response.Data;
+			}
+
+			if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+			{
+				_ticket = null;
+				return SendWithRetry(createRequest, send, true);
+			}
+
+			return response.Data;
+		}
+
+		public void SendWithRetry(Func<RestRequest> createRequest, Func<RestRequest, IRestResponse> send, bool isRetry = false)
+		{
+			RestRequest request = createRequest();
+			AddTicketHeader(request);
+			IRestResponse response = send(request);
+
+			if (isRetry)
+			{
+				return;
+			}
+
+			if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+			{
+				_ticket = null;
+				SendWithRetry(createRequest, send, true);
+			}
+		}
+
+		private void AddTicketHeader(RestRequest request)
+		{
+			if (_ticket == null || _ticket.AssumedValid() == false)
+			{
+				if (MakeValidTicket() == false)
+				{
+					throw new Exception("Unable to get ticket");
+				}
+			}
+
+			string ticket = Utilities.Ticket.CreateTicket(_userName, _password, _ip, _ticket.UseCount, _ticket.ValidFrom, _ticket.ValidTo);
+
+			#if DEBUG
+			Console.Out.WriteLine($"{request.Method}:{request.Resource} ticket:[{ticket}] -  UserName:{_userName}, Password:{_password}, Ip:{_ip}, UseCount:{_ticket.UseCount}, ValidFrom:{_ticket.ValidFrom}, ValidTo:{_ticket.ValidTo}");
+			#endif
+
+			request.AddHeader("Authorization", _userName + "|" + ticket);
+			_ticket.UseCount++;
+		}
+
+		private bool MakeValidTicket()
+		{
+			DateTime currentTime = DateTime.Now;
+
+			if (_ticket == null || _ticket.AssumedValid() == false)
+			{
+				_ticket = new Ticket()
+				{
+					ValidFrom = currentTime,
+					ValidTo = currentTime + _ticketLifeSpan,
+				};
+
+				bool ticketResponse = UpdateTicket(_ticket);
+
+				return ticketResponse;
+			}
+
+			return true;
+		}
+
+		private bool UpdateTicket(Ticket ticket)
+		{
+			RestRequest request = new RestRequest("Ticket", Method.PUT)
+			{
+				RequestFormat = DataFormat.Json,
+				JsonSerializer = new RestSharpDataContractJsonSerializer(),
+			};
+			request.AddBody(ticket);
+
+			request.AddHeader("Authorization", _userName + "|");
+
+			IRestResponse<bool> response = _client.Put<bool>(request);
+
+			if (response.StatusCode != System.Net.HttpStatusCode.OK)
+			{
+				Console.Out.WriteLine($"failed update ticket");
+				return false;
+			}
+
+			#if DEBUG
+			Console.Out.WriteLine($"ticket update {response.Data} {ticket.ValidFrom} - {ticket.ValidTo}");
+			#endif
+
+			return response.Data;
 		}
 	}
 }
