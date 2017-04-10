1c45dba0662514a5297a20bd29dc48580c671606
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-04-10 09:54:20 +0200

    (Utilities) added Ticket class

---
+++ kort/Utilities/Ticket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Utilities
+{
+	public static class Ticket
+	{
+		private const string dateFromat = "yyyy-MM-dd HH:mm:ss";
+
+		public static string CreateTicket(string UserName, string Password, string ip, int useCount, DateTime ValidFrom, DateTime ValidTo)
+		{
+			string unhashedString = $"{UserName}{Password}{ip}{useCount}{ValidFrom.ToString(dateFromat)}{ValidTo.ToString(dateFromat)}";
+
+			string hashedString = HashToMd5(unhashedString);
+
+			return hashedString;
+		}
+
+		public static string HashToMd5(string stringToHashMd5From)
+		{
+			byte[] unhashedBytes = System.Text.Encoding.UTF8.GetBytes(stringToHashMd5From);
+			byte[] hashedBytes = MD5.Create().ComputeHash(unhashedBytes);
+			//return BitConverter.ToString(hashedBytes);
+			return Convert.ToBase64String(hashedBytes);
+		}
+	}
+}
